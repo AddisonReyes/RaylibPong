ee26a57b050e696a71b547adeb788b8fa9f1f2b8
Addison Reyes <106483642+AddisonReyes@users.noreply.github.com>
2025-06-13 21:29:49 -0400

    The ball bounches

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,60 @@ using namespace std;
 const int screenWidth = 1200;
 const int screenHeight = 800;
 
+class Ball
+{
+public:
+    float x, y;
+    float speedX, speedY;
+    int radius;
+
+    void draw()
+    {
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void update()
+    {
+        x += speedX;
+        y += speedY;
+
+        if (y + radius >= GetScreenHeight() || y - radius < 0)
+        {
+            speedY *= -1;
+        }
+        if (x + radius >= GetScreenWidth() || x - radius < 0)
+        {
+            speedX *= -1;
+        }
+    }
+};
+
 int main()
 {
-    InitWindow(screenWidth, screenHeight, "My Pong Game!");
+    InitWindow(screenWidth, screenHeight, "Pong Game! My First Raylib game!!!");
     SetTargetFPS(60);
+
+    Ball ball;
+    ball.radius = 20;
+    ball.x = screenWidth / 2;
+    ball.y = screenHeight / 2;
+    ball.speedX = 7.0f;
+    ball.speedY = 7.0f;
 
     while (WindowShouldClose() == false)
     {
         BeginDrawing();
+
+        // Update
+        ball.update();
+
+        // Drawing
+        ClearBackground(BLACK);
+        DrawLine(screenWidth / 2, 0, screenWidth / 2, screenHeight, WHITE);
+        ball.draw();
+        DrawRectangle(10, screenHeight / 2 - 60, 25, 120, WHITE);
+        DrawRectangle(screenWidth - 35, screenHeight / 2 - 60, 25, 120, WHITE);
+
         EndDrawing();
     }
 
